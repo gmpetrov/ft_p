bf4aaa2d2755096b684674f9d19bc56a6e569a35
gmp89 <gm.petrov@yahoo.fr>
2014-05-13 19:33:04 +0200

    Video

--- client_dir/srcs/main.c
+++ client_dir/srcs/main.c
@@ -6,14 +6,46 @@
 /*   By: gpetrov <gpetrov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/13 18:17:46 by gpetrov           #+#    #+#             */
-/*   Updated: 2014/05/13 18:18:30 by gpetrov          ###   ########.fr       */
+/*   Updated: 2014/05/13 19:29:25 by gpetrov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include "client.h"
 
-int		main(void)
+int		create_client(char *addr, int port)
 {
-	ft_putstr("Je suis le client\n");
+	int					sock;
+	struct protoent		*proto;
+	struct sockaddr_in	sin;
+
+	proto = getprotobyname("tcp");
+	if (!proto)
+		return (-1);
+	sock = socket(PF_INET, SOCK_STREAM, proto->p_proto);
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(port);
+	sin.sin_addr.s_addr = inet_addr(addr);
+	connect(sock, (const struct sockaddr *)&sin, sizeof(sin));
+	return (sock);	
+}
+
+int		main(int ac, char **av)
+{
+	int					port;
+	int					sock;
+
+	if (ac != 3)
+	{
+		ft_putstr("[USAGE] - ./client [address] [port]\n");
+		return (0);
+	} 
+	port = ft_atoi(av[2]);
+	sock = create_client(av[1], port);
+	write(sock, "hello\n", 6);
+	close(sock);
 	return (0);
 }
--- serveur_dir/srcs/main.c
+++ serveur_dir/srcs/main.c
@@ -6,14 +6,59 @@
 /*   By: gpetrov <gpetrov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/13 18:20:57 by gpetrov           #+#    #+#             */
-/*   Updated: 2014/05/13 18:24:54 by gpetrov          ###   ########.fr       */
+/*   Updated: 2014/05/13 19:19:22 by gpetrov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include "serveur.h"
 
-int		main(void)
+int		create_server(int port)
 {
-	ft_putstr("Je suis le serveur\n");
+	int					sock;
+	struct protoent		*proto;
+	struct sockaddr_in	sin;
+
+	proto = getprotobyname("tcp");
+	if (!proto)
+		return (-1);
+	sock = socket(PF_INET, SOCK_STREAM, proto->p_proto);
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(port);
+	sin.sin_addr.s_addr = htonl(INADDR_ANY);
+	bind(sock, (const struct sockaddr *)&sin, sizeof(sin));
+	listen(sock, 42);
+	return (sock);	
+}
+
+int		main(int ac, char **av)
+{
+	int					port;
+	int					sock;
+	int					cs;
+	unsigned int		cslen;
+	struct sockaddr_in	csin;	
+	int					r;
+	char				buf[1024];
+
+	if (ac != 2)
+	{
+		ft_putstr("[USAGE] - ./serveur [port]\n");
+		return (0);
+	}
+	port = ft_atoi(av[1]);
+	sock = create_server(port);
+	cs = accept(sock, (struct sockaddr *)&csin, &cslen);
+	while ((r = read(cs, buf, 1023)) > 0)
+	{
+		buf[r] = 0;
+		ft_putstr(buf);
+		write(1, "\n", 1);
+	}
+	close(cs);
+	close(sock);
 	return (0);
 }
